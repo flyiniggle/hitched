66b7a21b1deba8a1c31bdbad53edf0635610926a
Dan Thompson <d.thompso@yahoo.com>
2017-03-12 18:28:36 -0400

    basic mongodb setup.

--- wsgi.py
+++ wsgi.py
@@ -1,11 +1,15 @@
 import os
 import cherrypy
+import pymongo
 
 import files
 
 
 class application(object):
     m = None
+    cursor = pymongo.MongoClient(os.environ['OPENSHIFT_MONGODB_DB_HOST'], int(os.environ['OPENSHIFT_MONGODB_DB_PORT']))
+    db = cursor.hitched
+    invitations = db.invitations
 
     @cherrypy.expose
     def index(self):
@@ -16,3 +20,14 @@ class application(object):
     @cherrypy.expose
     def default(self, *args, **kwargs):
         return self.index()
+
+    @cherrypy.expose
+    def guest_address(self, address):
+        guest = self.invitations.find_one({"addressLine1": address})
+        return guest
+
+    @cherrypy.expose
+    def guest_name(self, name):
+        guest = self.invitations.find_one({"First Name": name})
+        return guest
+
